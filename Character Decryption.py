e001b54e7fed4013ec570c6b57dd3355b9d3687e
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-21 11:18:10 +0800

    Added input commands

---
+++ Character Decryption.py
@@ -0,0 +1,8 @@
+#Pierre Victor T. Zurbito
+#Object Oriented Programming
+#BSCPE 1-4
+#Polytechnic University of the Philippines
+#PROBLEM 2
+
+#Allows the user to input a certain string
+input_string=input("Please input a string to decrypt:")
