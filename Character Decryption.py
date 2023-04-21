0e64ddc38d40b954971651192aa5b7dfcdafd74e
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-21 11:56:49 +0800

    Decryption for A

--- Character Decryption.py
+++ Character Decryption.py
@@ -6,3 +6,6 @@
 
 #Allows the user to input a certain string
 input_string=input("Please input a string to decrypt:")
+#String undergoes decryption
+if "*" in input_string:
+    string_a_decrypt=input_string.replace("*", "a")
