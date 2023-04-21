5206a1b457aae25f1c259c0cef09e6b005f37ac6
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-21 12:00:39 +0800

    Prints output string, fixes spacings

--- Character Decryption.py
+++ Character Decryption.py
@@ -6,6 +6,7 @@
 
 #Allows the user to input a certain string
 input_string=input("Please input a string to decrypt:")
+
 #String undergoes decryption
 if "*" in input_string:
     string_a_decrypt=input_string.replace("*", "a")
@@ -17,3 +18,6 @@ if "+" in string_i_decrypt:
     string_o_decrypt=string_i_decrypt.replace("+","o")
 if "!" in string_o_decrypt:
     output_string=string_o_decrypt.replace("!","u")
+
+#Prints the decrypted string
+print(output_string)
